a9e25915a4a9eee8d358e1e4194bee2b54a8ffa9
v310r <webmail200601@gmail.com>
2021-06-19 22:49:47 +0300

    final version of MyShared_ptr

--- MyShared_ptr.h
+++ MyShared_ptr.h
@@ -18,34 +18,12 @@ private:
     ControlUnit* unit;
     CObject* ptr;
 public:
-    MyShared_ptr(CObject* pointer):ptr(pointer),unit(new ControlUnit()){}
-    MyShared_ptr(const MyShared_ptr& other):ptr(other.ptr),unit(other.unit)
-    {
-        (unit->getCounter())++;
-    }
-    ~MyShared_ptr()
-    {
-        (unit->getCounter())--;
-        if(unit->getCounter() == 0)
-        {
-            delete unit;
-            delete ptr;
-        }
-    }
-    MyShared_ptr& operator=(const MyShared_ptr& other){
-        if(this == &other){return *this;}
-        ptr = other.ptr;
-        unit = other.unit;
-        (unit->getCounter())++;
-        return *this;
-    }
-CObject& operator*(){
-    return *ptr;
-}
-CObject* operator->(){
-    return ptr;
-}
-operator bool(){
-    return ptr;
-}
+    MyShared_ptr(CObject* pointer);
+    MyShared_ptr(const MyShared_ptr& other);
+    ~MyShared_ptr();
+    ControlUnit* getControlUnit();
+    MyShared_ptr& operator=(const MyShared_ptr& other);
+    CObject& operator*();
+    CObject* operator->();
+    operator bool();
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "MyUnique_ptr.h"
-//#include "MyShared_ptr.h"
+#include "MyShared_ptr.h"
 
 int main(){
     MyUnique_ptr smart1(new CObject);
@@ -11,6 +11,17 @@ int main(){
     else{
         std::cout<<"smart1 is null\n";
     }
+
+    MyShared_ptr groupSmart1(new CObject);
+    {
+        MyShared_ptr groupSmart2(groupSmart1);
+        {
+            MyShared_ptr groupSmart3(groupSmart2);
+            std::cout<<"Amount of shared_ptrs that indicate on obtained resource: "<<groupSmart3.getControlUnit()->getCounter()<<"\n\n";
+        }
+        std::cout<<"Amount of shared_ptrs that indicate on obtained resource: "<<groupSmart2.getControlUnit()->getCounter()<<"\n\n";
+    }
+    std::cout<<"Amount of shared_ptrs that indicate on obtained resource: "<<groupSmart1.getControlUnit()->getCounter()<<"\n\n";
 
     return 0;
 }
